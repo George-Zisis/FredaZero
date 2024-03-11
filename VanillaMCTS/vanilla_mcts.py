4ba2d264a8af1adbfa9b082ee10dd4923806d716
georgezisis <161433675+George-Zisis@users.noreply.github.com>
2024-03-11 09:26:54 +0200

    Update and rename mcts.py to vanilla_mcts.py
    Update mcts to work with updated chess env

--- VanillaMCTS/vanilla_mcts.py
+++ VanillaMCTS/vanilla_mcts.py
@@ -1,29 +1,29 @@
 import numpy as np
 import math
-from tqdm import tqdm
+
+from tqdm import trange
 
 
 class Node:
-	def __init__(self, game, args, state, parent=None, action_taken=None, action_idx=None):
+	def __init__(self, game, args, state, parent=None, action_idx=0, action_taken=None):
 		self.game = game
 		self.args = args
 		self.state = state
 		self.parent = parent
-		self.action_idx = action_idx  
+		self.action_idx = 0
 		self.action_taken = action_taken
 
 		self.children = []
-		self.expandable_moves = game.get_valid_moves(state)
-		self.expandable_moves_idx = np.ones(len(game.get_valid_moves(state)))
+		self.expandable_moves = np.ones(len(game.get_valid_actions(state)))
 
 		self.visit_count = 0
 		self.value_sum = 0
 
 	def is_fully_expanded(self):
-		return np.sum(self.expandable_moves_idx) == 0 and len(self.children) > 0
+		return np.sum(self.expandable_moves) == 0 and len(self.children) > 0
 
 	def select(self):
-		best_child = None 
+		best_child = None
 		best_ucb = -np.inf
 
 		for child in self.children:
@@ -39,29 +39,33 @@ class Node:
 		return q_value + self.args['C'] * math.sqrt(math.log(self.visit_count) / child.visit_count)
 
 	def expand(self):
-		action_idx = np.random.choice(np.where(self.expandable_moves_idx == 1)[0])
-		action = self.expandable_moves[action_idx]
-		self.expandable_moves_idx[action_idx] = 0
+		valid_actions = self.game.get_valid_actions(self.state)
+		action_idx = np.random.choice(np.where(self.expandable_moves == 1)[0])
+		self.expandable_moves[action_idx] = 0
+		action = valid_actions[action_idx]
 
-		self.game.board.push(action)
-		child = Node(self.game, self.args, self.game.board.fen(), self, action, action_idx)
+		child_state = self.state
+		child_state = self.game.get_next_state(child_state, action)
+
+		child = Node(self.game, self.args, child_state, self, action_idx, action)
 		self.children.append(child)
+
 		return child
 
 	def simulate(self):
-		value, is_terminal = self.game.get_value_and_terminated(self.state)
+		value, is_terminal = self.game.game_over(self.state)
 		if is_terminal:
 			return value
+
 		rollout_state = self.state
 		while True:
-			valid_moves = self.game.get_valid_moves(rollout_state)
-			action = np.random.choice(valid_moves)
-			self.game.board.push(action)
-			rollout_state = self.game.board.fen()
-			value, is_terminal = self.game.get_value_and_terminated(rollout_state)
+			value, is_terminal = self.game.game_over(rollout_state)
 			if is_terminal:
 				return value
-			
+			valid_moves = self.game.get_valid_actions(rollout_state)
+			action = np.random.choice(valid_moves)
+			rollout_state = self.game.get_next_state(rollout_state, action)
+
 	def backpropagate(self, value):
 		self.value_sum += value
 		self.visit_count += 1
@@ -78,13 +82,13 @@ class MCTS:
 	def search(self, state):
 		root = Node(self.game, self.args, state)
 
-		for search in tqdm(range(self.args['num_searches']), leave=True):
+		for search in trange(self.args['num_searches']):
 			node = root
+
 			while node.is_fully_expanded():
 				node = node.select()
-				self.game.board.push(node.action_taken)
 
-			value, is_terminal = self.game.get_value_and_terminated(node.state)
+			value, is_terminal = self.game.game_over(node.state)
 
 			if not is_terminal:
 				node = node.expand()
@@ -92,12 +96,9 @@ class MCTS:
 
 			node.backpropagate(value)
 
-			# Reset the game to initial state
-			self.game.new_game()
-			self.game.board.set_fen(state)
-
-		action_probs = np.zeros(len(self.game.get_valid_moves(state)))
+		action_probs = np.zeros(len(root.children))
 		for child in root.children:
 			action_probs[child.action_idx] = child.visit_count
-			action_probs = action_probs / np.sum(action_probs)
-			return action_probs
+		action_probs = action_probs / np.sum(action_probs)
+
+		return action_probs
